662aeed7fa6f7d173f10d0bc25736d2da630f61b
Neelesh7372 <neeleshsharma.kgp90@gmail.com>
2024-05-06 08:53:54 +0530

    adding code to metro.config

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,30 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+// const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+// module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+
+// import { getDefaultConfig } from 'metro-config';
+
+const {
+  resolver: {sourceExts, assetExts},
+} = await getDefaultConfig();
+
+const updatedConfig = {
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+  resolver: {
+    assetExts: assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
+  },
+};
+
+module.exports = updatedConfig;
